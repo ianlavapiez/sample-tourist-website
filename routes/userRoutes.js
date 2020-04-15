c2e72ac2913440af1a9813734f0caa0ef3f01541
ianlavapiez <ianlavapiez@gmail.com>
2020-04-15 22:06:04 +0800

    Routes: Added new login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController')
-const { signup } = require('../controllers/authController')
+const { signup, login } = require('../controllers/authController')
 
 const router = express.Router()
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
