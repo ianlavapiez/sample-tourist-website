7b33f433643605758f2ed648ab92b335ccaca1c1
ianlavapiez <ianlavapiez@gmail.com>
2020-04-21 23:06:22 +0800

    App: added sample middleware to read the headers

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
+app.use((req, res, next) => {
+  // console.log(req.headers)
+
+  next()
+})
+
 // 2. Routes
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
