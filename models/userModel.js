003319a44601119c3e58fb82daf2c06cad04d299
ianlavapiez <ianlavapiez@gmail.com>
2020-04-21 23:58:39 +0800

    User Model: added changedPasswordAfter methods to check for the jwt timestamp and changed password timestamp

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       message: 'Password are not the same!',
     },
   },
+  passwordChangedAt: Date,
 })
 
 userSchema.pre('save', async function (next) {
@@ -48,6 +49,17 @@ userSchema.pre('save', async function (next) {
 
 userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
   return await bcrypt.compare(candidatePassword, userPassword)
+}
+
+userSchema.methods.changedPasswordAfter = function (jwtTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10)
+
+    return jwtTimestamp < changedTimestamp
+  }
+
+  // false means not changed
+  return false
 }
 
 const User = mongoose.model('User', userSchema)
