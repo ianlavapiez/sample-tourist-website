b7f045ed11d177fd3bf7992843665e0c6bfb9e92
ianlavapiez <ianlavapiez@gmail.com>
2020-04-21 23:59:13 +0800

    Error Controller: added additional error handling if statements

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,6 +21,10 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400)
 }
 
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401)
+
+const handleJWTExpiredError = () => new AppError('Your token has expired! Please log in again!', 401)
+
 const sendErrorForDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -63,6 +67,8 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'CastError') error = handleCastErrorDB(error)
     if (error.code === 11000) error = handleDuplicateFieldsDB(error)
     if (error.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (error.name === 'JsonWebTokenError') error = handleJWTError()
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError()
 
     sendErrorForProd(error, res)
   }
