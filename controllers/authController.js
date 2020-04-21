6bf91fd596dbd4c3a628c8a5fdd29520af6adcae
ianlavapiez <ianlavapiez@gmail.com>
2020-04-21 23:58:04 +0800

    Auth Controller: updated the protect function to filter login JWT token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
@@ -12,12 +13,13 @@ const signToken = (id) => {
 }
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const { name, email, password, passwordConfirm } = req.body
+  const { name, email, password, passwordConfirm, passwordChangedAt } = req.body
   const newUser = await User.create({
     name,
     email,
     password,
     passwordConfirm,
+    passwordChangedAt,
   })
   const token = signToken(newUser._id)
 
@@ -58,16 +60,21 @@ exports.protect = catchAsync(async (req, res, next) => {
     token = req.headers.authorization.split(' ')[1]
   }
 
-  console.log(token)
+  if (!token) return next(new AppError('You are not logged in! Please log in to get access!', 401))
 
-  if (!token) {
-    return next(new AppError('You are not logged in! Please log in to get access!', 401))
-  }
   // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
 
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id)
+
+  if (!currentUser) return next(new AppError('The token belonging to this token does not longer exists.', 401))
 
   // 4) Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(new AppError('User recently changed password, please log in again!', 401))
+  }
 
+  req.user = currentUser
   next()
 })
