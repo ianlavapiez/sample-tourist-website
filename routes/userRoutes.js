b90022dc084efd8719aad1fd8e2a8c94345dbdec
ianlavapiez <ianlavapiez@gmail.com>
2020-04-22 13:19:08 +0800

    added update-password route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,29 @@
 const express = require('express')
 
-const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController')
-const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController')
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController')
+const {
+  signup,
+  login,
+  protect,
+  updatePassword,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/authController')
 
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
 
+router.patch('/update-password', protect, updatePassword)
 router.post('/forgot-password', forgotPassword)
-router.post('/reset-password', resetPassword)
+router.patch('/reset-password/:token', resetPassword)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
