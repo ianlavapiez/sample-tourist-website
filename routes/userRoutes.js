fa0ac56323764dc8fb0d7e329d59461b767c3506
ianlavapiez <ianlavapiez@gmail.com>
2020-04-22 11:41:51 +0800

    User Routes: added forgotPassword and resetPassword functions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 
 const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/userController')
-const { signup, login } = require('../controllers/authController')
+const { signup, login, forgotPassword, resetPassword } = require('../controllers/authController')
 
 const router = express.Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
+
+router.post('/forgot-password', forgotPassword)
+router.post('/reset-password', resetPassword)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
