dace47a20d0cb5c9bfa8ea35d0e260a1e2707411
ianlavapiez <ianlavapiez@gmail.com>
2020-04-22 23:05:34 +0800

    User Controller: added updateMe function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,17 @@
 const User = require('../models/userModel')
 
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+const filterObject = (object, ...allowedFields) => {
+  const newObject = {}
+
+  Object.keys(object).forEach((el) => {
+    if (allowedFields.includes(el)) newObject[el] = object[el]
+  })
+
+  return newObject
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find()
@@ -10,6 +21,31 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  })
+})
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError('This route is not for password updates. Please use /update-password.', 400)
+    )
+  }
+
+  // 2) Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObject(req.body, 'name', 'email')
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   })
 })
