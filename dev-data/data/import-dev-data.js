b0710e2f3edf6923de8d90499a5995f55f43eadb
ianlavapiez <ianlavapiez@gmail.com>
2020-04-23 18:55:18 +0800

    Dev Data: added tours.json and deleted the recent tours in mongoDB

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,7 +20,7 @@ mongoose
   .then(() => console.log('DB connection successful!'))
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'))
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'))
 
 // IMPORT DATA TO DATABASE
 const importData = async () => {
