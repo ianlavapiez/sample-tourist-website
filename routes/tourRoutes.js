20fd1cc15d57fc619e2958bd77a3539c6b4f9487
ianlavapiez <ianlavapiez@gmail.com>
2020-04-23 19:24:49 +0800

    Tour Routes: checked the usage of restrictTo function

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -18,6 +18,10 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
 router.route('/tour-stats').get(getTourStats)
 router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router.route('/').get(protect, getAllTours).post(createTour)
-router.route('/:id').get(getTour).patch(updateTour).delete(protect, restrictTo('admin', 'lead-guide'), deleteTour)
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour)
 
 module.exports = router
