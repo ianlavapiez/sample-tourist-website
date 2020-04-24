f4b980a97fa4727c62fd70a01665ea7f781d6e17
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 14:09:01 +0800

    Review Controllers: added filter object in getting all reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,16 +1,14 @@
 const Review = require('../models/reviewModel')
 
-const APIFeatures = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
+  let filter = {}
+
+  if (req.params.tourId) filter = { tour: req.params.tourId }
+
   // EXECUTE QUERY
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-  const reviews = await features.query
+  const reviews = await Review.find(filter)
 
   // SEND RESPONSE
   res.status(200).json({
@@ -23,6 +21,10 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 })
 
 exports.createReview = catchAsync(async (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId
+  if (!req.body.user) req.body.user = req.user.id
+
   const newReview = await Review.create(req.body)
 
   res.status(201).json({
