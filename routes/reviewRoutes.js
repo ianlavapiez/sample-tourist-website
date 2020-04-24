b153bedd6ae18c22ac08144690e027b7a61b1b32
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 14:39:43 +0800

    Review Routes: added delete route for reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const { getAllReviews, createReview } = require('../controllers/reviewController')
+const { getAllReviews, createReview, deleteReview } = require('../controllers/reviewController')
 const { protect, restrictTo } = require('../controllers/authController')
 
 const router = express.Router({ mergeParams: true })
 
 router.route('/').get(protect, getAllReviews).post(protect, restrictTo('user'), createReview)
+router.route('/:id').delete(deleteReview)
 
 module.exports = router
