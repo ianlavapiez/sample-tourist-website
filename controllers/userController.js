c72505e1148e630e041e83d89ac97b86f163c3e5
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 14:39:20 +0800

    User Controller: changed the delete exports to factory deleteOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel')
 
+const { deleteOne } = require('./handlerFactory')
+
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 
@@ -80,9 +82,4 @@ exports.updateUser = (req, res) => {
   })
 }
 
-exports.deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  })
-}
+exports.deleteUser = deleteOne(User)
