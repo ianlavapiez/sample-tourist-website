6326355dc3af2d609eb4a339c606d602942e896f
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 14:10:01 +0800

    Review Routes: settings the router's mergeParams to true

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { getAllReviews, createReview } = require('../controllers/reviewController')
 const { protect, restrictTo } = require('../controllers/authController')
 
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 
 router.route('/').get(protect, getAllReviews).post(protect, restrictTo('user'), createReview)
 
