a7b55a928fd358be9b92d4ad1602d576219c165e
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 15:45:05 +0800

    Handle Factory Controller: added retrieveOne and retrieveAll

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
+const APIFeatures = require('../utils/apiFeatures')
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -43,5 +44,49 @@ exports.deleteOne = (Model) =>
     res.status(204).json({
       status: 'success',
       data: null,
+    })
+  })
+
+exports.retrieveOne = (Model, populateOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id)
+
+    if (populateOptions) query = query.populate(populateOptions)
+
+    const doc = await query
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404))
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    })
+  })
+
+exports.retrieveAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    let filter = {}
+
+    if (req.params.tourId) filter = { tour: req.params.tourId }
+
+    // EXECUTE QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const docs = await features.query
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        data: docs,
+      },
     })
   })
