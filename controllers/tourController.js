61b8d6808c5d5a1f3a566c29030f7d1aa4b3876e
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 14:39:04 +0800

    Tour Controller: changed the delete exports to factory deleteOne

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,7 @@
 const Tour = require('../models/tourModel')
 
+const { deleteOne } = require('./handlerFactory')
+
 const APIFeatures = require('../utils/apiFeatures')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
@@ -71,18 +73,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   })
 })
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id)
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404))
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  })
-})
+exports.deleteTour = deleteOne(Tour)
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
