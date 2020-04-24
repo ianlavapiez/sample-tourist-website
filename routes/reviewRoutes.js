dfe9beba3be4918cb36e309dfe10577db07fb2e1
ianlavapiez <ianlavapiez@gmail.com>
2020-04-24 15:00:13 +0800

    Review Routes: added middleware function for getting tour and user ids upon creating a review

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express')
 
-const { getAllReviews, createReview, deleteReview } = require('../controllers/reviewController')
+const {
+  getAllReviews,
+  createReview,
+  updateReview,
+  deleteReview,
+  setTourUserIds,
+} = require('../controllers/reviewController')
 const { protect, restrictTo } = require('../controllers/authController')
 
 const router = express.Router({ mergeParams: true })
 
-router.route('/').get(protect, getAllReviews).post(protect, restrictTo('user'), createReview)
-router.route('/:id').delete(deleteReview)
+router
+  .route('/')
+  .get(protect, getAllReviews)
+  .post(protect, restrictTo('user'), setTourUserIds, createReview)
+router.route('/:id').patch(updateReview).delete(deleteReview)
 
 module.exports = router
