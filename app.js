b3933ab7052825947d5d0ccba7c6b1b6555e0777
ianlavapiez <ianlavapiez@gmail.com>
2020-04-25 19:44:42 +0800

    App.js: added render method to '/' route

--- app.js
+++ app.js
@@ -73,7 +73,10 @@ app.use((req, res, next) => {
 
 // 2. Routes
 app.get('/', (req, res) => {
-  res.status(200).render('base')
+  res.status(200).render('base', {
+    tour: 'The Park Camper',
+    user: 'Ian',
+  })
 })
 
 app.use('/api/v1/tours', tourRouter)
