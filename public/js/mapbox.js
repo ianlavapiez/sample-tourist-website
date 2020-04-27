efd7ea08d22344e89efaccb950dcf5bb9cf616c6
ianlavapiez <ianlavapiez@gmail.com>
2020-04-28 00:54:26 +0800

    Public: modified mapbox export

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,46 +1,47 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById('map').dataset.locations)
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoibHVmZnlwaXJhdGVzIiwiYSI6ImNqeG56d2QzNDAxcXozZXA0dWpndjFlMXUifQ._9zhfl0f2WVh5_shJbJZ4A'
-var map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/luffypirates/ck9gydzd82fq11iqgxu58iycn',
-  scrollZoom: false,
-})
-
-const bounds = new mapboxgl.LngLatBounds()
-
-locations.forEach((location) => {
-  // Create marker
-  const element = document.createElement('div')
-  element.className = 'marker'
-
-  // Add marker
-  new mapboxgl.Marker({
-    element,
-    anchor: 'bottom',
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoibHVmZnlwaXJhdGVzIiwiYSI6ImNqeG56d2QzNDAxcXozZXA0dWpndjFlMXUifQ._9zhfl0f2WVh5_shJbJZ4A'
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/luffypirates/ck9gydzd82fq11iqgxu58iycn',
+    scrollZoom: false,
   })
-    .setLngLat(location.coordinates)
-    .addTo(map)
 
-  // Add pop-up
-  new mapboxgl.Popup({
-    offset: 30,
+  const bounds = new mapboxgl.LngLatBounds()
+
+  locations.forEach((location) => {
+    // Create marker
+    const element = document.createElement('div')
+    element.className = 'marker'
+
+    // Add marker
+    new mapboxgl.Marker({
+      element,
+      anchor: 'bottom',
+    })
+      .setLngLat(location.coordinates)
+      .addTo(map)
+
+    // Add pop-up
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(location.coordinates)
+      .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+      .addTo(map)
+
+    // Extends map bounds to include current location
+    bounds.extend(location.coordinates)
   })
-    .setLngLat(location.coordinates)
-    .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
-    .addTo(map)
 
-  // Extends map bounds to include current location
-  bounds.extend(location.coordinates)
-})
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-})
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  })
+}
