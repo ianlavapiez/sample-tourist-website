2d31fcaf13d64d108322cb6d25d6866bafd3e65e
ianlavapiez <ianlavapiez@gmail.com>
2020-04-28 17:03:05 +0800

    Public: added new functions for logout

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import '@babel/polyfill'
 import { displayMap } from './mapbox'
-import { login } from './login'
+import { login, logout } from './login'
 
 // DOM elements
 const mapBox = document.getElementById('map')
 const loginForm = document.querySelector('.form')
+const logoutBtn = document.querySelector('.nav__el--logout')
 
 // Delegation
 if (mapBox) {
@@ -24,3 +25,5 @@ if (loginForm) {
     login(email, password)
   })
 }
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout)
