0b635a4db13e1f69ed45117aeaf7187adbb4b20c
ianlavapiez <ianlavapiez@gmail.com>
2020-04-29 01:00:47 +0800

    Controller: modified reviewController

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,16 +1,16 @@
-const Review = require('../models/reviewModel')
-
-const { createOne, retrieveAll, retrieveOne, updateOne, deleteOne } = require('./handlerFactory')
-
-exports.getAllReviews = retrieveAll(Review)
-exports.getReview = retrieveOne(Review)
-exports.createReview = createOne(Review)
-exports.updateReview = updateOne(Review)
-exports.deleteReview = deleteOne(Review)
+const Review = require('./../models/reviewModel')
+const factory = require('./handlerFactory')
+// const catchAsync = require('./../utils/catchAsync');
 
 exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId
   if (!req.body.user) req.body.user = req.user.id
-
   next()
 }
+
+exports.getAllReviews = factory.getAll(Review)
+exports.getReview = factory.getOne(Review)
+exports.createReview = factory.createOne(Review)
+exports.updateReview = factory.updateOne(Review)
+exports.deleteReview = factory.deleteOne(Review)
