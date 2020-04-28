c0f3af9e2128121f7bbb816145ced0592f8f5a9c
ianlavapiez <ianlavapiez@gmail.com>
2020-04-29 01:01:00 +0800

    Controller: modified errorController

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,32 +1,30 @@
-const AppError = require('../utils/appError')
+const AppError = require('./../utils/appError')
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`
-
   return new AppError(message, 400)
 }
 
 const handleDuplicateFieldsDB = (err) => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
-  const message = `Duplicate field value: ${value}. Please use another value!`
 
+  const message = `Duplicate field value: ${value}. Please use another value!`
   return new AppError(message, 400)
 }
 
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message)
 
-  const message = `Invalid input data. ${errors.join(' ')}`
-
+  const message = `Invalid input data. ${errors.join('. ')}`
   return new AppError(message, 400)
 }
 
 const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401)
 
 const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please log in again!', 401)
+  new AppError('Your token has expired! Please log in again.', 401)
 
-const sendErrorForDev = (err, req, res) => {
+const sendErrorDev = (err, req, res) => {
   // A) API
   if (req.originalUrl.startsWith('/api')) {
     return res.status(err.statusCode).json({
@@ -37,60 +35,62 @@ const sendErrorForDev = (err, req, res) => {
     })
   }
 
-  // B) Rendered Website
+  // B) RENDERED WEBSITE
+  console.error('ERROR 💥', err)
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
-    message: err.message,
+    msg: err.message,
   })
 }
 
-const sendErrorForProd = (err, req, res) => {
-  // API
-  // Operational, trusted error: send message to client
+const sendErrorProd = (err, req, res) => {
+  // A) API
   if (req.originalUrl.startsWith('/api')) {
+    // A) Operational, trusted error: send message to client
     if (err.isOperational) {
       return res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
       })
     }
+    // B) Programming or other unknown error: don't leak error details
     // 1) Log error
-    console.error('ERROR', err)
-
+    console.error('ERROR 💥', err)
     // 2) Send generic message
     return res.status(500).json({
       status: 'error',
-      message: 'Something went wrong.',
+      message: 'Something went very wrong!',
     })
   }
 
-  // Rendered Website
+  // B) RENDERED WEBSITE
+  // A) Operational, trusted error: send message to client
   if (err.isOperational) {
     return res.status(err.statusCode).render('error', {
       title: 'Something went wrong!',
-      message: err.message,
+      msg: err.message,
     })
-    // Programming or other unknown error: don't leak error details
   }
+  // B) Programming or other unknown error: don't leak error details
   // 1) Log error
-  console.error('ERROR', err)
-
+  console.error('ERROR 💥', err)
   // 2) Send generic message
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
-    message: 'Please try again later.',
+    msg: 'Please try again later.',
   })
 }
 
 module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorForDev(err, req, res)
+    sendErrorDev(err, req, res)
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err }
-
     error.message = err.message
 
     if (error.name === 'CastError') error = handleCastErrorDB(error)
@@ -99,6 +99,6 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'JsonWebTokenError') error = handleJWTError()
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError()
 
-    sendErrorForProd(error, req, res)
+    sendErrorProd(error, req, res)
   }
 }
