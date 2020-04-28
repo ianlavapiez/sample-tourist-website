c86759afeb6b7867dd08314052501240ac4131fc
ianlavapiez <ianlavapiez@gmail.com>
2020-04-29 00:55:34 +0800

    Routes: added tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,37 +1,61 @@
-const express = require('express')
+const express = require('express');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
-const reviewRouter = require('./reviewRoutes')
+const router = express.Router();
 
-const {
-  getAllTours,
-  getTour,
-  getTourStats,
-  getMonthlyPlan,
-  createTour,
-  updateTour,
-  deleteTour,
-  aliasTopTours,
-  getToursWithin,
-  getDistances,
-} = require('../controllers/tourController')
-const { protect, restrictTo } = require('../controllers/authController')
+// router.param('id', tourController.checkID);
 
-const router = express.Router()
+// POST /tour/234fad4/reviews
+// GET /tour/234fad4/reviews
 
-router.use('/:tourId/reviews', reviewRouter)
+router.use('/:tourId/reviews', reviewRouter);
 
-router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
-router.route('/tour-stats').get(getTourStats)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
 router
   .route('/monthly-plan/:year')
-  .get(protect, restrictTo('admin', 'lead-guide', 'guide'), getMonthlyPlan)
-router.route('/').get(getAllTours).post(protect, restrictTo('admin', 'lead-guide'), createTour)
-router.route('/distances/:latlng/unit/:unit').get(getDistances)
-router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(getToursWithin)
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
+
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+// /tours-within?distance=233&center=-40,45&unit=mi
+// /tours-within/233/center/-40,45/unit/mi
+
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
+
 router
   .route('/:id')
-  .get(getTour)
-  .patch(protect, restrictTo('admin', 'lead-guide'), updateTour)
-  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour)
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
+    tourController.updateTour
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
-module.exports = router
+module.exports = router;
