d272e00d67f033110629c4988b407fba92540047
ianlavapiez <ianlavapiez@gmail.com>
2020-04-29 01:02:26 +0800

    Routes: modified reviewRoutes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,24 +1,30 @@
-const express = require('express')
+const express = require('express');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
 
-const {
-  getAllReviews,
-  getReview,
-  createReview,
-  updateReview,
-  deleteReview,
-  setTourUserIds,
-} = require('../controllers/reviewController')
-const { protect, restrictTo } = require('../controllers/authController')
+const router = express.Router({ mergeParams: true });
 
-const router = express.Router({ mergeParams: true })
+router.use(authController.protect);
 
-router.use(protect)
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(
+    authController.restrictTo('user'),
+    reviewController.setTourUserIds,
+    reviewController.createReview
+  );
 
-router.route('/').get(getAllReviews).post(restrictTo('user'), setTourUserIds, createReview)
 router
   .route('/:id')
-  .get(getReview)
-  .patch(restrictTo('user', 'admin'), updateReview)
-  .delete(restrictTo('user', 'admin'), deleteReview)
+  .get(reviewController.getReview)
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
-module.exports = router
+module.exports = router;
