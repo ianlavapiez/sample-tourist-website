f6f131a72e815ad7742201ea908f528519c4ff9d
ianlavapiez <ianlavapiez@gmail.com>
2020-04-30 21:26:13 +0800

    Controller: removed console.logs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -36,7 +36,6 @@ const sendErrorDev = (err, req, res) => {
   }
 
   // B) RENDERED WEBSITE
-  console.error('ERROR 💥', err)
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
     msg: err.message,
@@ -55,7 +54,6 @@ const sendErrorProd = (err, req, res) => {
     }
     // B) Programming or other unknown error: don't leak error details
     // 1) Log error
-    console.error('ERROR 💥', err)
     // 2) Send generic message
     return res.status(500).json({
       status: 'error',
@@ -73,7 +71,6 @@ const sendErrorProd = (err, req, res) => {
   }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
-  console.error('ERROR 💥', err)
   // 2) Send generic message
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
@@ -82,8 +79,6 @@ const sendErrorProd = (err, req, res) => {
 }
 
 module.exports = (err, req, res, next) => {
-  // console.log(err.stack);
-
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
 
