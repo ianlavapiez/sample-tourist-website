943745281f8270933e52d8dfc1b242dd606f5db1
ianlavapiez <ianlavapiez@gmail.com>
2020-05-01 21:40:53 +0800

    Controller: removed comments

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const multer = require('multer')
 const sharp = require('sharp')
+
 const Tour = require('./../models/tourModel')
 const catchAsync = require('./../utils/catchAsync')
 const factory = require('./handlerFactory')
