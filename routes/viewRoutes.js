4834bb3581a69efa4241d4639d16e55fb6a8381f
ianlavapiez <ianlavapiez@gmail.com>
2020-05-02 23:31:38 +0800

    Routes: re-structured the code

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const viewsController = require('../controllers/viewsController');
-const authController = require('../controllers/authController');
+const express = require('express')
 
-const router = express.Router();
+const viewsController = require('../controllers/viewsController')
+const authController = require('../controllers/authController')
 
-router.use(viewsController.alerts);
+const router = express.Router()
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.use(viewsController.alerts)
 
-router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
-router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
-router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/', authController.isLoggedIn, viewsController.getOverview)
 
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour)
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm)
+router.get('/me', authController.protect, viewsController.getAccount)
 
-router.post(
-  '/submit-user-data',
-  authController.protect,
-  viewsController.updateUserData
-);
+router.get('/my-tours', authController.protect, viewsController.getMyTours)
 
-module.exports = router;
+router.post('/submit-user-data', authController.protect, viewsController.updateUserData)
+
+module.exports = router
